84fd5a1ce7b8b7f8089724f7c94c94061ef68e7c
migu55 <miguelmarasigan55@gmail.com>
2025-10-20 15:27:58 -0700

    corner chat buttons

--- app/layout.py
+++ app/layout.py
@@ -1,10 +1,13 @@
 from nicegui import ui
 from app.components.sidebar import sidebar
+from app.components.addchat import buttons
 
 def with_sidebar(page_content):
     with ui.row().classes('w-full h-screen'):
         sidebar()
+        buttons()
 
         # main content
         with ui.column().classes('flex-1 p-6 overflow-auto'):
             page_content()
+
