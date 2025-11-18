571d04eff114e85676de58357d6fe24f1fd62500
migu55 <miguelmarasigan55@gmail.com>
2025-11-17 17:17:11 -0800

    Important Dates and minor tweaks

--- dbmodule/calendardata.py
+++ dbmodule/calendardata.py
@@ -2,66 +2,80 @@ from enum import Enum
 
 # data tables
 class Event(Enum):
-	TNAME = "event"
-	DATE = "date"
-	DESC = "description"
-	DETAIL = "detail"
-	
+    TNAME = "event"
+    DATE = "date"
+    DESC = "description"
+    DETAIL = "detail"
+
 class CalendarData:
-	def __init__(self, sqlCursor):
-		self.cursor = sqlCursor
-		return None
-	
-	def buildData(self):
-		query = (f"create table if not exists {Event.TNAME.value}"
-		f"({Event.DATE.value} Real primary key,"
-		f"{Event.DESC.value} text,"
-		f"{Event.DETAIL.value} text)")
-		
-		print(query)
-		#self.cursor.execute(query)
-		return None
-	
-	def addData(self, date, description, detail):
-		query = (f"insert into {Event.TNAME.value} "
-		f"({Event.DATE.value}, {Event.DESC.value}, {Event.DETAIL.value}) "
-		f"values ({date}, {description}, {detail})")
-		
-		print(query)
-		#self.cursor.execute(query)
-		return None
+    def __init__(self, sqlCursor):
+        self.cursor = sqlCursor
+        return None
 
-	def updateDescription(self, desc, date):
-		query = (f"update {Event.TNAME.value} "
-		f"set ({Event.DESC.value} = {desc} "
-		f"where {Event.DATE.value} = {date})")
-		
-		print(query)
-		#self.cursor.execute(query)
-		return None
+    def buildData(self):
+        query = (f"create table if not exists {Event.TNAME.value}"
+        f"({Event.DATE.value} Real primary key,"
+        f"{Event.DESC.value} text,"
+        f"{Event.DETAIL.value} text)")
 
-	def updateDetail(self, desc, date):
-		query = (f"update {Event.TNAME.value} "
-		f"set ({Event.DESC.value} = {desc} "
-		f"where {Event.DATE.value} = {date})")
-		
-		print(query)
-		#self.cursor.execute(query)
-		return None
-	
-	def updateDate(self, oldDate, newDate):
-		query = (f"select {Event.DESC.value}, {Event.DETAIL.value} from {Event.TNAME.value} "
-		f"where {Event.DATE.value} = {oldDate})")
-		print(query)
-		#result = self.cursor.execute(query)
-		desc = None
-		detail = None
-		#desc, detail = result.fetchone()
-		
-		query = (f"delete from {Event.TNAME.value} "
-		f"where {Event.DATE.value} = {oldDate})")
-		print(query)
-		#self.cursor.execute(query)
-		
-		self.addData(newDate, desc, detail)
-		return None
+        print(query)
+        #self.cursor.execute(query)
+        return None
+
+    def addData(self, date, description, detail):
+        query = (f"insert into {Event.TNAME.value} "
+        f"({Event.DATE.value}, {Event.DESC.value}, {Event.DETAIL.value}) "
+        f"values ({date}, {description}, {detail})")
+
+        print(query)
+        #self.cursor.execute(query)
+        return None
+
+    def updateDescription(self, desc, date):
+        query = (f"update {Event.TNAME.value} "
+        f"set ({Event.DESC.value} = {desc} "
+        f"where {Event.DATE.value} = {date})")
+
+        print(query)
+        #self.cursor.execute(query)
+        return None
+
+    def updateDetail(self, desc, date):
+        query = (f"update {Event.TNAME.value} "
+        f"set ({Event.DESC.value} = {desc} "
+        f"where {Event.DATE.value} = {date})")
+
+        print(query)
+        #self.cursor.execute(query)
+        return None
+
+    def updateDate(self, oldDate, newDate):
+        query = (f"select {Event.DESC.value}, {Event.DETAIL.value} from {Event.TNAME.value} "
+        f"where {Event.DATE.value} = {oldDate})")
+        print(query)
+        #result = self.cursor.execute(query)
+        desc = None
+        detail = None
+        #desc, detail = result.fetchone()
+
+        query = (f"delete from {Event.TNAME.value} "
+        f"where {Event.DATE.value} = {oldDate})")
+        print(query)
+        #self.cursor.execute(query)
+
+        self.addData(newDate, desc, detail)
+        return None
+
+    def findEventsInRangeMainCal(self, oldDate, newDate):
+        query = f"select * from {Event.TNAME.value} where {Event.DATE.value} between {oldDate} and {newDate};)"
+        #self.cursor.execute(query).fetchall()
+        #TODO: SORT VALUES INTO DICTIONARY FORM, key: day, value: list[events]
+        d = {"a": [1, 2, 3]}
+        return d
+
+    def findEventsInRangeImpDate(self, oldDate, newDate):
+        query = f"select * from {Event.TNAME.value} where {Event.DATE.value} between {oldDate} and {newDate};)"
+        #self.cursor.execute(query).fetchall()
+        #TODO: SORT VALUES INTO DICTIONARY FORM, key: day, value: list[events]
+        d = {"a": [1, 2, 3]}
+        return d
